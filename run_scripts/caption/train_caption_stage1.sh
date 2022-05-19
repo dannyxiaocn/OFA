1f6b1ccb305b09593481851821cb47e96b418271
dannyxiaocn <bochengxiao@icloud.com>
2022-05-19 18:33:01 +0800

    update gitignore

--- run_scripts/caption/train_caption_stage1.sh
+++ run_scripts/caption/train_caption_stage1.sh
@@ -2,10 +2,10 @@
 
 # The port for communication. Note that if you want to run multiple tasks on the same machine,
 # you need to specify different port numbers.
-export MASTER_PORT=1051
+export MASTER_PORT=4051
 
-log_dir=./stage1_logs
-save_dir=./stage1_checkpoints
+log_dir=./baseline1_stage1_logs
+save_dir=./baseline1_stage1_checkpoints
 mkdir -p $log_dir $save_dir
 
 bpe_dir=../../utils/BPE
@@ -13,7 +13,7 @@ user_dir=../../ofa_module
 
 data_dir=../../dataset/caption_data
 data=${data_dir}/caption_stage1_train.tsv,${data_dir}/caption_val.tsv
-restore_file=./stage1_checkpoints/2_0.06_2500/checkpoint_last.pt
+restore_file=../../checkpoints/ofa_base.pt
 selected_cols=0,4,2
 
 task=caption
@@ -24,7 +24,7 @@ lr=1e-5
 max_epoch=5
 warmup_ratio=0.06
 batch_size=8
-update_freq=4
+update_freq=8
 resnet_drop_path_rate=0.0
 encoder_drop_path_rate=0.1
 decoder_drop_path_rate=0.1
@@ -37,7 +37,7 @@ patch_image_size=480
 eval_cider_cached=${data_dir}/cider_cached_tokens/coco-valid-words.p
 drop_worst_ratio=0.2
 
-for max_epoch in {3,}; do
+for max_epoch in {2,}; do
   echo "max_epoch "${max_epoch}
   for warmup_ratio in {0.06,}; do
     echo "warmup_ratio "${warmup_ratio}
@@ -48,7 +48,7 @@ for max_epoch in {3,}; do
       save_path=${save_dir}/${max_epoch}"_"${warmup_ratio}"_"${drop_worst_after}
       mkdir -p $save_path
 
-      CUDA_VISIBLE_DEVICES=0 python3 -m torch.distributed.launch --nproc_per_node=1 --master_port=${MASTER_PORT} ../../train.py \
+      CUDA_VISIBLE_DEVICES=3 python3 -m torch.distributed.launch --nproc_per_node=1 --master_port=${MASTER_PORT} ../../train.py \
           $data \
           --selected-cols=${selected_cols} \
           --bpe-dir=${bpe_dir} \
@@ -84,10 +84,10 @@ for max_epoch in {3,}; do
           --fixed-validation-seed=7 \
           --no-epoch-checkpoints --keep-best-checkpoints=1 \
           --save-interval=1 --validate-interval=1 \
-          --save-interval-updates=5000 --validate-interval-updates=500 \
+          --save-interval-updates=2000 --validate-interval-updates=2000 \
           --eval-cider \
           --eval-cider-cached-tokens=${eval_cider_cached} \
-          --eval-args='{"beam":5,"max_len_b":16,"no_repeat_ngram_size":3}' \
+          --eval-args='{"beam":1,"max_len_b":16,"no_repeat_ngram_size":3}' \
           --best-checkpoint-metric=cider --maximize-best-checkpoint-metric \
           --max-src-length=${max_src_length} \
           --max-tgt-length=${max_tgt_length} \
